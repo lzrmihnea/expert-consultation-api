15cf7bc4af44e453cddb03a25333d8e2150930cf
Nicolae Stihhi <nicolae.stihhi@gmail.com>
2020-12-29 14:11:50 +0200

    Feature/214 include replies in comments api (#215)
    214 include replies in comments api (#215)

--- src/main/java/com/code4ro/legalconsultation/comment/controller/DocumentCommentsController.java
+++ src/main/java/com/code4ro/legalconsultation/comment/controller/DocumentCommentsController.java
@@ -7,6 +7,7 @@ import com.code4ro.legalconsultation.comment.model.dto.CommentForChatDto;
 import com.code4ro.legalconsultation.comment.model.persistence.Comment;
 import com.code4ro.legalconsultation.comment.service.CommentService;
 import com.code4ro.legalconsultation.core.model.dto.PageDto;
+import com.code4ro.legalconsultation.core.model.persistence.BaseEntity;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -17,7 +18,9 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/documentnodes/{nodeId}/comments")
@@ -64,7 +67,15 @@ public class DocumentCommentsController {
     public ResponseEntity<PageDto<CommentForChatDto>> findAll(@ApiParam(value = "The id of the node") @PathVariable final UUID nodeId,
                                                              @ApiParam("Page object information being requested") final Pageable pageable) {
         Page<Comment> comments = commentService.findAll(nodeId, pageable);
-        Page<CommentForChatDto> commentsDto = comments.map(commentMapper::mapToCommentForChatDto);
+        List<UUID> commentIds = comments.map(BaseEntity::getId).getContent();
+        List<Comment> allReplies = commentService.findRepliesForComments(commentIds);
+        Page<CommentForChatDto> commentsDto = comments.map(comment -> {
+            List<Comment> repliesForComment = allReplies
+                    .stream()
+                    .filter(r -> r.getParent().getId().equals(comment.getId()))
+                    .collect(Collectors.toList());
+            return commentMapper.mapToCommentForChatDto(comment, repliesForComment);
+        });
 
         return ResponseEntity.ok(new PageDto<>(commentsDto));
     }
